b68ddaa6432973fbe06007674838d10315da3465
kittu3G <mehvishkanha192@gmail.com>
2024-02-18 11:55:05 +0530

    new project

---
+++ react-app/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+       {/* HEADER */}
+       <Header></Header>
+
+       {/* MIDPART  */}
+       <MidPart></MidPart>
+
+       {/* TENDING-PRODUCT  */}
+       <TrendProduct></TrendProduct>
+
+       {/* MIDPART  */}
+       <MidPart></MidPart>
+
+       {/* ALL-PRODUCT  */}
+       <AllProduct></AllProduct>
+
+       {/* SERVICE  */}
+       <AppService></AppService>
+
+       {/* FOOTER  */}
+       <Footer></Footer>
+
+
+
+    </div>
+    )
+    
+  }
+
+export default App;
